3355ae20c96a7cedc1e44cb26743091eb71265ae
Edwin Prakash <edwin.prakash@tec.govt.nz>
2024-01-31 15:24:49 +1300

    Initial commit

---
+++ src/Proxy/Log.cs
@@ -0,0 +1,47 @@
+﻿namespace Proxy;
+
+internal sealed partial class ClusterSelectorMiddleware
+{
+    private static class Log
+    {
+        private static readonly Action<ILogger, string, Exception?> _noFeatureFoundOrFeatureDisabled = LoggerMessage.Define<string>(
+            LogLevel.Information,
+            EventIds.NoFeatureFoundOrFeatureDisabled,
+            "Feature '{featureName}' is diabled or does not exist.");
+
+        private static readonly Action<ILogger, string, Exception?> _featureEnabled = LoggerMessage.Define<string>(
+            LogLevel.Information,
+            EventIds.FeatureEnabled,
+            "Feature '{featureName}' is enabled.");
+
+        private static readonly Action<ILogger, string, Exception?> _noClusterFound = LoggerMessage.Define<string>(
+            LogLevel.Error,
+            EventIds.NoClusterFound,
+            "Cluster '{clusterName}' not found.");
+
+        private static readonly Action<ILogger, string, Exception?> _clusterReassigned = LoggerMessage.Define<string>(
+            LogLevel.Information,
+            EventIds.ClusterReassigned,
+            "Cluster reassigned from default to '{clusterName}'.");
+
+        public static void NoFeatureFoundOrFeatureDisabled(ILogger logger, string featureName)
+        {
+            _noFeatureFoundOrFeatureDisabled(logger, featureName, null);
+        }
+
+        public static void FeatureEnabled(ILogger logger, string featureName)
+        {
+            _featureEnabled(logger, featureName, null);
+        }
+
+        public static void NoClusterFound(ILogger logger, string clusterName)
+        {
+            _noClusterFound(logger, clusterName, null);
+        }
+
+        public static void ClusterReassigned(ILogger logger, string clusterName)
+        {
+            _clusterReassigned(logger, clusterName, null);
+        }
+    }
+}
---
+++ src/WebApplication.Old/Controllers/TideForecastController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Old.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TideForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "High", "Low", "King"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public TideForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTide")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(0, 49),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
